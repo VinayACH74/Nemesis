22cc340c12e2688cac4cf57d132e64594267970c
VinayACH <vinaychaudhari1901@gmail.com>
2022-01-08 20:49:40 +0530

    some changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,20 @@ app.use(cookies());
 app.use(express.json());
 app.use("/", require("./router/adminAuth"));
 
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
   app.listen(PORT, () => {
